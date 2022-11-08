1268acd97470d2b679c44fb8a04d603ea366b033
Daniel C. Lechok <dclechok@gmail.com>
2022-11-08 16:29:03 -0500

    integrated electron, fleshing out UI for address list

---
+++ src/AddressListBuilder.js
@@ -0,0 +1,55 @@
+import './AddressListBuilder.css';
+import addresses from './config/addresses.json'; //inventory of all addresses
+import { useState } from 'react';
+
+function AddressListBuilder() {
+
+    const [siteChecked, setSiteChecked] = useState();
+
+    return (
+
+    <div className="address-box">
+        <ul>
+            {
+                Object.keys(addresses).map((listItem, key) => {
+                    return (<><li id={listItem} key={key}><input type="checkbox" value={listItem}/><label htmlFor={listItem}>{listItem}</label></li></>)
+                })
+            }
+        </ul>
+    {/* <input type="checkbox" />
+    <label>Midland, PA</label><br />
+    &nbsp;&nbsp;<input type="checkbox" />
+    <label htmlFor="all-ips">Rack Switches</label><br />
+
+
+    &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>MDC01</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.1.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.2.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.3.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.4.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.5.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>10.1.6.200</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>MDC02</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>MDC03</label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" />
+    <label>MDC04</label><br />
+
+    &nbsp;&nbsp;<input type="checkbox" />
+    <label htmlFor="all-ips">Core Switches</label><br />
+    <hr />
+    <input type="checkbox" />
+    <label>Sharon, PA</label><br /> */}
+    </div>  
+    );
+}
+
+export default AddressListBuilder;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './App.css';
 
+import AddressListBuilder from './AddressListBuilder';
+
 const { ipcRenderer } = window.require('electron');
 
 function App() {
 
   const handleMin = (e) => {
     const { id } = e.currentTarget;
-    if(id === "min") ipcRenderer.send('minimize');
+    if (id === "min") ipcRenderer.send('minimize');
   }
 
   const handleClose = () => {
-    if(window.confirm('Are you sure you wish to exit?')) ipcRenderer.send('exit-app');
+    if (window.confirm('Are you sure you wish to exit?')) ipcRenderer.send('exit-app');
   }
 
   return (
     <div className="App">
       <header><h1>Legacy Crawler</h1></header>
       <div className="close-button-container">
-      <button className="remove-btn-style" onClick={handleMin} id="min">[▼]</button>&nbsp;
-      <button className="remove-btn-style" onClick={handleClose} id="close">[X]</button>
+        <button className="remove-btn-style" onClick={handleMin} id="min">[▼]</button>&nbsp;
+        <button className="remove-btn-style" onClick={handleClose} id="close">[X]</button>
       </div>
-        <p>tEST</p>
+      <div className='ip-selection-container'>
+        <div className='loaded-ips'>
+          <AddressListBuilder />
+        </div>
+        <div className='add-remove-ips'>
+          <button>&gt;</button>
+          <button>&lt;</button>
+        </div>
+        <div className='selected-ips'>
+          <p>This is selection container</p>
+        </div>
+
+      </div>
+
     </div>
   );
 }
