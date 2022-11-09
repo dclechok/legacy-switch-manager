fc350a073c22e27cee25c5168bc00c5f6c7fa0d3
Daniel C. Lechok <dclechok@gmail.com>
2022-11-09 16:30:00 -0500

    comments building out queue box

--- src/QueueBuilder.js
+++ src/QueueBuilder.js
@@ -2,6 +2,8 @@ import './QueueBuilder.css';
 
 function QueueBuilder({ queue }) {
     console.log(queue)
+    //build out queue box of selected IPs pre-pushing config
+    //validate that we're not adding duplicates, or devices from conflicting areas
     return (
         <ul className="que-box">
             {queue && queue.map(qItem => <li>{qItem}</li>)}
