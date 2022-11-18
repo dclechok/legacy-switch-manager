0f2e44c12b5a0479cebb396fc962328e36158dc4
Daniel C. Lechok <dclechok@gmail.com>
2022-11-18 10:18:57 -0500

    sorted que list from lowest IP to highest IP for ease of finding IPs

--- src/QueueBuilder.js
+++ src/QueueBuilder.js
@@ -13,8 +13,17 @@ function QueueBuilder({ queue, selected, setSelected }) {
     return (
         <div>
             {queue && <p className='total-in-que'>Total Devices in Queue: <span style={{fontWeight: "bold"}}>{queue.size}</span></p>}
-        <select id="que-builder" class="form-select que-box" multiple aria-label="multiple select example size 5" onChange={handleSelected}>
-            {queue && Array.from(queue).map(qItem => <option>{qItem}</option>)}
+        <select id="que-builder" className="form-select que-box" multiple aria-label="multiple select example size 5" onChange={handleSelected}>
+            {queue && Array.from(queue).map((qItem, key) => <option key={key}>{qItem}</option>).sort((a, b) => {
+                const destructureA = a.props.children.split('.'); // ['10', '2', '13', '200']
+                const destructureB = b.props.children.split('.'); // ['10', '7', '3', '200']
+                console.log(destructureA[1], destructureB[1])
+                // console.log(Number(destructureA) - Number(destructureB));
+                if(Number(destructureA[1]) > Number(destructureB[1])) return 1;
+                if(Number(destructureA[1]) < Number(destructureB[1])) return -1;
+                if(Number(destructureA[2]) > Number(destructureB[2])) return 1;
+                if(Number(destructureA[2]) < Number(destructureB[2])) return -1;
+            })}
         </select>
         </div>
     )
