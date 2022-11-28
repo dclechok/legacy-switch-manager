273086920ac2d2366fff45692b110c291cc939ed
Daniel C. Lechok <dclechok@gmail.com>
2022-11-28 08:52:56 -0500

    fixed select options for configloader component

--- src/ConfigLoader.js
+++ src/ConfigLoader.js
@@ -1,18 +1,26 @@
 import './ConfigLoader.css';
+import { useState } from 'react';
 
 function ConfigLoader() {
-
+    const [selectedConfig, setSelectedConfig] = useState('--select config--')
     //"stock" configurations will auto-detect device model/version for applying the correct config (between 3750 and 2960 cisco switches)
+
+    const handleSelect = (e) => {
+        setSelectedConfig(e.currentTarget.value);
+    };
+
+    console.log(selectedConfig);
+
     return (
         <div className="config-menu">
             <p>Choose Configuration:</p>
-            <select>
-                <option>--select config--</option>
-                <option id="stock">Mawson Stock</option>
-                <option id="password">Change Password</option>
-                <option>Custom...</option>
+            <select value={selectedConfig} onChange={handleSelect}>
+                <option id="default" defaultValue="--select config--">--select config--</option>
+                <option id="stock" value="Mawson Stock">Mawson Stock</option>
+                <option id="password" value="Change Password">Change Password</option>
+                <option id="custom" value="Custom...">Custom...</option>
             </select><br /><br />
-            <button>Push Configuration</button>
+            <button id="push-config">Push Configuration</button>
         </div>
     );
 }
--- src/OutputLog.js
+++ src/OutputLog.js
@@ -4,33 +4,33 @@ import { useState } from 'react';
 // import doSSH from './ssh/nodeSsh';
 const BASE_URL = "http://localhost:5000/";
 
-function OutputLog(){
+function OutputLog() {
 
-    const [outputStream, setOutputStream] = useState();
+  // const [outputStream, setOutputStream] = useState();
 
-    // setOutputStream(doSSH);export async '
-    async function serverCall() {
-        try {
-         const response = await fetch(BASE_URL + "ssh", {
-            method: "GET",
-            headers: {
-            "Content-Type": "application/json",
-      },
-    //   body: JSON.stringify({ data: { test: 'terst'} })
-    });
-    const jsonResponse = await response.json(); //json-ify readablestream data
-    if (jsonResponse) return jsonResponse;
-  } catch (e) {
-    console.log(e, "Failed to fetch all assets.");
+  // setOutputStream(doSSH);export async '
+  async function serverCall() {
+    try {
+      const response = await fetch(BASE_URL + "ssh", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: { test: 'terst'} })
+      });
+      const jsonResponse = await response.json(); //json-ify readablestream data
+      if (jsonResponse) return jsonResponse;
+    } catch (e) {
+      console.log(e, "Failed to make PUT request.");
+    }
   }
-}
-    serverCall();
+  serverCall();
 
-    return (
+  return (
     <div className="output-log">
-        <p>Output</p>
+      <p>Output</p>
     </div>
-);
+  );
 }
 
 export default OutputLog;
