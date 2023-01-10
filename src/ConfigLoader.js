61dde6d555a154539b1e555c1da1d892d4d18192
Daniel C. Lechok <dclechok@gmail.com>
2023-01-10 14:13:50 -0500

    removed select options for configurations, integrating an input field to allow uploads of txt formatted configurations

--- src/ConfigLoader.js
+++ src/ConfigLoader.js
@@ -3,18 +3,34 @@ import { useState } from 'react';
 
 function ConfigLoader({ queue }) {
     const BASE_URL = "http://localhost:5000/";
-    const [selectedConfig, setSelectedConfig] = useState('--select config--')
-    //"stock" configurations will auto-detect device model/version for applying the correct config (between 3750 and 2960 cisco switches)
+    const [config, setConfig] = useState();
+    const [clickVal, setClickVal] = useState();
+    const [stockConf, setStockConf] = useState(false);
 
-    const handleSelect = (e) => {
-        setSelectedConfig(e.currentTarget.value);
+    const handleConfigSubmit= (e) => {
+        e.preventDefault();
+        //validate that we have a text file
+        console.log(e.currentTarget.id);
+        if(e.target.id === 'stop') window.alert('Stopped!');
+        else if(e.currentTarget.id === 'push'){
+            window.confirm('Lyra does not validate your configuration! Please check that you have uploaded a valid configuration. Click OK if you wish to proceed. (Read documentation to see how configurations need to be formatted.)');
+            console.log(e.target);
+            // setConfig(e.target[0].file);
+            // var fileReader = new FileReader();
+            // fileReader.readAsText(config);
+        }
+
     };
+
+    const handleConfigClick = (e) => {
+        setClickVal(e.currentTarget.id);
+    }
 
     const handleConfigPush = (e) => {
         //validate if we want to push this configuration for sure (and queue is not empty)
         //assuming all switches selected in queue have the same username / password - enter username / password
-        console.log(queue);
-        if(queue.size === 0) return window.alert("Queue is empty!");
+        if (queue.size === 0) return window.alert("Queue is empty!");
+        if (config === "--select config--") return window.alert("A configuration must be selected!");
         async function serverCall() {
             try {
                 const response = await fetch(BASE_URL + "ssh", {
@@ -22,7 +38,12 @@ function ConfigLoader({ queue }) {
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify({ data: { test: 'terst' } })
+                    body: JSON.stringify({
+                        data: {
+                            stock: stockConf, //true or false if stock configuration is selected
+                            queue: Array.from(queue) //turn set of queued items into an array
+                        }
+                    })
                 });
                 const jsonResponse = await response.json(); //json-ify readablestream data
                 if (jsonResponse) return jsonResponse;
@@ -34,17 +55,13 @@ function ConfigLoader({ queue }) {
     };
 
 
-
     return (
         <div className="config-menu">
-            <p>Choose Configuration:</p>
-            <select value={selectedConfig} onChange={handleSelect}>
-                <option id="default" defaultValue="--select config--">--select config--</option>
-                <option id="stock" value="Mawson Stock">Mawson Stock</option>
-                <option id="password" value="Change Password">Change Password</option>
-                <option id="custom" value="Custom...">Custom...</option>
-            </select><br /><br />
-            <button id="push-config" onClick={handleConfigPush}>Push Configuration</button>
+            <form>
+                <input type="file" name="new-config" accept=".txt" /><br /><br />
+                <button id="push" onClick={handleConfigSubmit}>Push Configuration</button>
+                <button id="stop" onClick={handleConfigSubmit}>Stop</button>
+            </form>
         </div>
     );
 }
